1201e665cde2e91643b17457e7fa6652ec6fdd59
Alex Liu <mingzhangliu@gmail.com>
2026-04-19 19:56:07 +0800

    Add initial project files: .gitignore, data.yaml, dataset downloader, and model scripts

---
+++ download_dataset.py
@@ -0,0 +1,8 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("andy8744/playing-cards-object-detection-dataset")
+
+print("Path to dataset files:", path)
+
+# C:\Users\mingz\.cache\kagglehub\datasets\andy8744\playing-cards-object-detection-dataset\versions\4
