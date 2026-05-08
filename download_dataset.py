ad15b72a41fcb4ebb0cfcbeb15b58e9880cc54ea
Alex Liu <mingzhangliu@gmail.com>
2026-05-08 12:08:54 +0800

    Remove commented-out path from dataset download script

--- download_dataset.py
+++ download_dataset.py
@@ -4,5 +4,3 @@ import kagglehub
 path = kagglehub.dataset_download("andy8744/playing-cards-object-detection-dataset")
 
 print("Path to dataset files:", path)
-
-# C:\Users\mingz\.cache\kagglehub\datasets\andy8744\playing-cards-object-detection-dataset\versions\4
